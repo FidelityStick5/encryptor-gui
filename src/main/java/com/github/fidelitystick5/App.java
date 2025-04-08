774c431ed23a9115c747b2bfb6bbb18402c7b36a
FidelityStick5 <79571925+FidelityStick5@users.noreply.github.com>
2025-04-08 21:36:19 +0200

    Implement encryptor logic

--- src/main/java/com/github/fidelitystick5/App.java
+++ src/main/java/com/github/fidelitystick5/App.java
@@ -1,12 +1,39 @@
 package com.github.fidelitystick5;
 
+import java.io.File;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    private void encrypt(File directory) {
+        final File[] files = directory.listFiles();
+        final BlockingQueue<File> queue = new ArrayBlockingQueue<>(files.length + 1);
+        final EncryptorThread[] threads = new EncryptorThread[4];
+
+        final int shift = 1;
+        final String header = "__ENCRYPTED__\n";
+
+        for (File file : files) {
+            try {
+                queue.put(file);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (EncryptorThread thread : threads) {
+            thread = new EncryptorThread(queue, header, shift);
+            thread.start();
+        }
+    }
 
     @Override
     public void start(Stage primaryStage) {
@@ -15,6 +42,24 @@ public class App extends Application {
         grid.setPadding(new Insets(16));
 
         Scene scene = new Scene(grid, 300, 200);
+
+        Button button = new Button("Choose directory to encrypt files");
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Open Resource File");
+
+        button.setOnAction(e -> {
+            File selectedDirectory = directoryChooser.showDialog(primaryStage);
+
+            if (selectedDirectory != null) {
+                encrypt(selectedDirectory);
+                return;
+            }
+
+            System.out.println("No directory selected.");
+        });
+
+        grid.add(button, 0, 0);
 
         primaryStage.setTitle("JavaFX Application");
         primaryStage.setScene(scene);
---
+++ src/main/java/com/github/fidelitystick5/EncryptorThread.java
@@ -0,0 +1,83 @@
+package com.github.fidelitystick5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+
+public class EncryptorThread extends Thread {
+  private BlockingQueue<File> queue;
+  private String header;
+  private int shift;
+
+  public EncryptorThread(BlockingQueue<File> queue, String header, int shift) {
+    this.queue = queue;
+    this.header = header;
+    this.shift = shift;
+  }
+
+  private String readFile(File file) throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(file));
+    StringBuilder builder = new StringBuilder();
+
+    try {
+      while (reader.ready()) {
+        char character = (char) reader.read();
+        builder.append(character);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      reader.close();
+    }
+
+    return builder.toString();
+  }
+
+  private void encryptFile(File file, String data, String header, int shift) throws IOException {
+    BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+    try {
+      final boolean isEncrypted = data.startsWith(header);
+
+      if (!isEncrypted)
+        writer.write(header);
+
+      char[] characters = data.substring(isEncrypted ? header.length() : 0).toCharArray();
+
+      for (char character : characters) {
+        char encryptedCharacter = (char) (character + (isEncrypted ? -shift : shift));
+        writer.write(encryptedCharacter);
+      }
+
+      System.out.println(String.format("%s %s file: %s",
+          Thread.currentThread().getName(),
+          isEncrypted ? "decrypted" : "encrypted",
+          file.getName()));
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      writer.close();
+    }
+  }
+
+  @Override
+  public void run() {
+    try {
+      while (true) {
+        if (queue.isEmpty())
+          break;
+
+        File file = queue.take();
+        String data = readFile(file);
+
+        encryptFile(file, data, header, shift);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
