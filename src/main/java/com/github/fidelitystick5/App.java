8c14415aa7996fd9f5673617adc2b0a8c7a0a2b8
FidelityStick5 <79571925+FidelityStick5@users.noreply.github.com>
2025-04-08 21:19:15 +0200

    Add JavaFX dependencies

--- src/main/java/com/github/fidelitystick5/App.java
+++ src/main/java/com/github/fidelitystick5/App.java
@@ -1,7 +1,27 @@
 package com.github.fidelitystick5;
 
-public class App {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        GridPane grid = new GridPane();
+
+        grid.setPadding(new Insets(16));
+
+        Scene scene = new Scene(grid, 300, 200);
+
+        primaryStage.setTitle("JavaFX Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
     }
 }
